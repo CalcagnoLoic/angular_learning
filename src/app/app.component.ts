764a17f3fbc5736537fd153db86248f38a934199
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-09-08 19:37:18 +0200

    fin de la partie sur les components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap_model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular_learning';
+export class AppComponent implements OnInit {
+    mySnap!: FaceSnap;
+    mySecondSnap!: FaceSnap;
+    myOtherSnap! : FaceSnap
+
+    ngOnInit(): void {
+        this.mySnap = new FaceSnap(
+            'Archibald',
+            'Mon meilleur ami depuis des années',
+            new Date(),
+            6,
+            'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+            'Like it!',
+            false
+        );
+        this.mySecondSnap = new FaceSnap(
+            'Starling bird',
+            'Une photo d\'un étourneau sansonnet',
+            new Date(),
+            2,
+            'https://cdn.pixabay.com/photo/2023/08/29/19/09/starling-8221990_960_720.jpg',
+            'Like it!',
+            false
+        );
+        this.myOtherSnap = new FaceSnap(
+            'Lynx',
+            'Un Lynx sauvage',
+            new Date(),
+            15,
+            'https://cdn.pixabay.com/photo/2023/09/02/15/03/lynx-8229077_960_720.png',
+            'Like it!',
+            false
+        );
+    }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap_model';
 
 @Component({
-  selector: 'app-face-snap',
-  templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.css']
+    selector: 'app-face-snap',
+    templateUrl: './face-snap.component.html',
+    styleUrls: ['./face-snap.component.css'],
 })
 export class FaceSnapComponent implements OnInit {
+    @Input() faceSnap!: FaceSnap;
 
-  constructor() { }
+    title!: string;
+    description!: string;
+    createdDate!: Date;
+    snaps!: number;
+    imageUrl!: string;
+    btnText!: string;
+    isClicked!: boolean;
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    onSnap() {
+        if (this.faceSnap.isClicked === true) {
+            this.faceSnap.snaps++;
+            this.faceSnap.btnText = 'Already liked';
+            this.faceSnap.isClicked = false;
+        } else {
+            this.faceSnap.snaps--;
+            this.faceSnap.btnText = 'Like it!';
+            this.faceSnap.isClicked = true;
+        }
+    }
 }
