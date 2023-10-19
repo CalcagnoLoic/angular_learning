074aed4aa8590dc597339263f3835bba7f30b0bb
Loïc Calcagno <calcagnoloic93@gmail.com>
2023-10-19 14:19:40 +0200

    ajout de guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { appRoutingModule } from './app.routing-module';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
     declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { LandingPageModule } from './landing-page/landing-page.module';
         HttpClientModule,
         CoreModule,
         LandingPageModule,
+        AuthModule
     ],
     bootstrap: [AppComponent],
 })
